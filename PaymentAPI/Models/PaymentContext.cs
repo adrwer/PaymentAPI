0546e6ee3deee6331321b2498c716ffa47be451a
Adrian Werimo <adrwer7@gmail.com>
2021-10-15 17:24:01 +0300

    Project setup

---
+++ PaymentAPI/Models/PaymentContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentContext:DbContext
+    {
+        public PaymentContext(DbContextOptions<PaymentContext> options):base(options)
+        {
+        }
+
+        public DbSet<PaymentDetail> PaymentDetails { get; set; }
+    }
+}
